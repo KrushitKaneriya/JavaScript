cff161d935f1c70470f8f2c21793d58670ffd26d
Krushit Kaneriya <krushitkaneriya@gmail.com>
2025-02-18 15:31:08 +0530

    Add files via upload

---
+++ conversion.js
@@ -0,0 +1,52 @@
+// let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+
+
+// let isloggedin = 0
+
+// let booleanisloggedin = Boolean(isloggedin)
+// console.log(booleanisloggedin);
+
+
+
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);
+
+
+
+// let str1 = "hello"
+// let str2 = " world"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+
+// console.log("1" + 2 );
+// console.log(1 + "2" );
+// console.log("1" + 2 + 2 );
+// console.log(1 + 2 + "2" );
+
+
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+
+
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
---
+++ datatypes.js
@@ -0,0 +1,17 @@
+// ##### Primitive Datatypes #####
+
+// 1. Number          const score = 100
+// 2. String          const score = "100"
+// 3. Boolean         const score = true
+// 4. null            const score = null
+// 5. undefined       const score = undefined
+// 6. Symbol
+// 7. BigInt          const bigInt = 1234567890123456789012345678901234567890n
+
+// ##### Reference(Non-Primitive) Datatypes #####
+
+// 1. Arrays            const heros = ["thor", "spiderman", "ironman"]
+// 2. Objects           const heros = {name: "thor", age: 1000}
+// 3. Functions         const heros = function(){console.log("hello")}
+
+
